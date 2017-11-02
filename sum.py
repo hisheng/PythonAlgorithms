ebbbbf7231bd3fe0c277c34694da9550f0ee8199
张海生 <465908774@qq.com>
2017-11-02 12:32:04 +0800

    一组数据求和,子序列求和

--- sum.py
+++ sum.py
@@ -11,3 +11,19 @@ def sum(arr):
 
 
 sum(arr)
+
+
+#求 子序列 最大的和 （百度面试）
+
+def subsum(arr):
+    total = 0
+    for index,a in enumerate(arr):
+        sub_sum = 0
+        for j in arr[index:] :
+            sub_sum += j
+        if sub_sum > total :
+            total = sub_sum
+    print(total)
+
+
+subsum(arr)
